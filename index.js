8ccde39f47454d5dee97e36f03bb00d772a10fe7
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-03-17 17:37:35 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.round(Math.random() * 10);
+console.log (multiply);
+console.log (random);
+const num3 = 22;
+const num4 = 6;
+const mod = num3 % num4;
+console.log (mod);
+const max = Math.max (11,5,7,20,15);
+console.log(max);
